4f10dd1172a9f01beba50c8405216093d3a81b14
zaki <zikoubouregbi@gmail.com>
2023-05-22 14:12:24 +0100

    first commit

---
+++ app/reserve/[slug]/page.tsx
@@ -0,0 +1,21 @@
+import Header from "./component/Header"
+import Form from "./component/Form"
+
+
+export default function Reserve  (){
+    return (
+
+    <div className="border-t h-screen">
+      <div className="py-9 w-3/5 m-auto">
+
+        <Header />
+
+       <Form />
+
+      </div>
+    </div>
+
+
+
+    )
+}
